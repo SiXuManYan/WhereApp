4e4fba159e50572b8deb2b5229d073c1df9b417f
zhuangYuFeiHelloGit <zhuangHelloWorld@163.com>
2020-12-03 16:56:29 +0800

    添加收藏酒店的API，给每个API补充注释 author:zyf

--- app/src/main/java/com/jcs/where/api/RetrofitApi.java
+++ app/src/main/java/com/jcs/where/api/RetrofitApi.java
@@ -4,25 +4,52 @@ import com.jcs.where.api.response.BannerResponse;
 import com.jcs.where.api.response.CategoryResponse;
 import com.jcs.where.api.response.HotelResponse;
 import com.jcs.where.api.response.ModulesResponse;
+import com.jcs.where.api.response.SuccessResponse;
 
 import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RetrofitApi {
 
 
+    /**
+     * 获得首页金刚圈的信息
+     * @return 金刚圈
+     */
     @GET("commonapi/v1/modules")
     Observable<List<ModulesResponse>> getModules();
 
+    /**
+     * 获得分类列表
+     * @param level 分类级别
+     * @param categories parentId 上级分类的id
+     * @return 分类列表
+     */
     @GET("commonapi/v1/categories")
     Observable<List<CategoryResponse>> getCategories(@Query("level") int level, @Query("pid") int[] categories);
 
+    /**
+     * 酒店猜你喜欢的酒店列表
+     * @return 酒店列表
+     */
     @GET("hotelapi/v1/hotels/recommends")
     Observable<List<HotelResponse>> getYouLike();
 
+    /**
+     * 获得轮播图
+     * @param type 区分是哪个页面的轮播图
+     * @return 轮播图
+     */
     @GET("commonapi/v1/banners")
     Observable<List<BannerResponse>> getBanners(@Query("type") int type);
+
+    /**
+     * 收藏酒店
+     */
+    @POST("hotelapi/v1/collects")
+    Observable<SuccessResponse> postCollectHotel(@Query("hotel_id") String hotelId);
 }

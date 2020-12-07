19efef654687b01ffdab013f74950d4514b1c7c5
zhuangYuFeiHelloGit <zhuangHelloWorld@163.com>
2020-12-07 10:20:38 +0800

    JcsTitle组件添加BottomLine author:zyf

---
+++ app/src/main/java/com/jcs/where/home/model/HotelCommentsModel.java
@@ -0,0 +1,4 @@
+package com.jcs.where.home.model;
+
+public class HotelCommentsModel {
+}
--- app/src/main/java/com/jcs/where/widget/JcsTitle.java
+++ app/src/main/java/com/jcs/where/widget/JcsTitle.java
@@ -15,7 +15,9 @@ import com.jcs.where.R;
 public class JcsTitle extends ConstraintLayout {
     private String middleTitle = "";
     private boolean showBackIv = true;
+    private boolean showBottomLine = false;
     private ImageView mBackIv;
+    private View mBottomLine;
     private TextView mMiddleTitleTv;
 
     public JcsTitle(Context context) {
@@ -31,6 +33,7 @@ public class JcsTitle extends ConstraintLayout {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.JcsTitle);
         middleTitle = array.getString(R.styleable.JcsTitle_middleTitle);
         showBackIv = array.getBoolean(R.styleable.JcsTitle_showBackIv, true);
+        showBottomLine = array.getBoolean(R.styleable.JcsTitle_showBottomLine, false);
 
         array.recycle();
         initView(context);
@@ -38,12 +41,17 @@ public class JcsTitle extends ConstraintLayout {
 
     private void initView(Context context) {
         View view = LayoutInflater.from(context).inflate(R.layout.widget_jcs_title, this);
+        mBottomLine = view.findViewById(R.id.bottomLine);
         mBackIv = view.findViewById(R.id.backIv);
         mMiddleTitleTv = view.findViewById(R.id.middleTitle);
         mMiddleTitleTv.setText(middleTitle);
 
         if (!showBackIv) {
             mBackIv.setVisibility(GONE);
+        }
+
+        if (showBottomLine) {
+            mBottomLine.setVisibility(VISIBLE);
         }
     }
 

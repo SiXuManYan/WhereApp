2391c2eb1cc56d21f83e885b9ec20e3e472fdf0d
SunCB <sunchengbo.08@163.com>
2021-01-21 17:56:41 +0800

    feat:积分签到UI

---
+++ app/src/main/java/com/jcs/where/integral/IntegralActivity.java
@@ -0,0 +1,43 @@
+package com.jcs.where.integral;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.jcs.where.R;
+import com.jcs.where.base.BaseActivity;
+
+/**
+ * 积分签到
+ */
+public class IntegralActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_integral;
+    }
+
+
+    @Override
+    protected void initView() {
+
+
+    }
+
+    @Override
+    protected void bindListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+
+
+
+
+}

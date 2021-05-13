fb47814b1d9b4197869c3f7897e1f1af7fbc9a33
wangsw <itshiwei@aliyun.com>
2021-05-13 14:23:11 +0800

    refactor:城市选择

--- app/src/main/java/com/jcs/where/hotel/activity/CityPickerActivity.java
+++ app/src/main/java/com/jcs/where/hotel/activity/CityPickerActivity.java
@@ -3,32 +3,23 @@ package com.jcs.where.hotel.activity;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Bundle;
+import android.location.Address;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.gson.Gson;
 import com.jcs.where.R;
 import com.jcs.where.adapter.CityListAdapter;
 import com.jcs.where.api.BaseObserver;
 import com.jcs.where.api.ErrorResponse;
-import com.jcs.where.api.HttpUtils;
 import com.jcs.where.api.response.CityPickerResponse;
-import com.jcs.where.base.BaseActivity;
-import com.jcs.where.base.CustomProgressDialog;
+import com.jcs.where.base.mvp.BaseMvpActivity;
 import com.jcs.where.bean.CityResponse;
-import com.jcs.where.bean.ErrorBean;
-import com.jcs.where.bean.GoogleMapBean;
 import com.jcs.where.bean.LocateState;
+import com.jcs.where.hotel.activity.picker.CityPickerPresenter;
+import com.jcs.where.hotel.activity.picker.CityPickerView;
 import com.jcs.where.hotel.model.CityPickerModel;
+import com.jcs.where.utils.LocationUtil;
+import com.jcs.where.utils.PermissionUtils;
 import com.jcs.where.utils.PinyinUtils;
 import com.jcs.where.utils.SPKey;
 import com.jcs.where.utils.SPUtil;
@@ -41,28 +32,17 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-
 /**
  * 城市选择页面
  */
-public class CityPickerActivity extends BaseActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class CityPickerActivity extends BaseMvpActivity<CityPickerPresenter> implements CityPickerView {
 
 
     public static final String EXTRA_CITY = "city";
     public static final String EXTRA_CITYID = "cityId";
-    public static CustomProgressDialog dialog;
-    private final int READ_CODE = 10;
-    private final int READ_LOCATIONCODE = 11;
     private ListView mListView;
     private SideLetterBar mLetterBar;
     private CityListAdapter mCityAdapter;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
-    private LatLng lastLatLng, perthLatLng;
-    private boolean mAddressRequested;
     private CityPickerModel mModel;
 
     @Override
@@ -78,22 +58,6 @@ public class CityPickerActivity extends BaseActivity implements GoogleApiClient.
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-        mGoogleApiClient.connect();
-        initView();
-        initData();
-        checkIsGooglePlayConn();
-    }
-
-    @Override
     protected int getLayoutId() {
         return R.layout.cp_activity_city_list;
     }
@@ -104,15 +68,48 @@ public class CityPickerActivity extends BaseActivity implements GoogleApiClient.
         TextView overlay = findViewById(R.id.tv_letter_overlay);
         mLetterBar = findViewById(R.id.side_letter_bar);
         mLetterBar.setOverlay(overlay);
-        mLetterBar.setOnLetterChangedListener(new SideLetterBar.OnLetterChangedListener() {
-            @Override
-            public void onLetterChanged(String letter) {
-                int position = mCityAdapter.getLetterPosition(letter);
-                mListView.setSelection(position);
-            }
+        mLetterBar.setOnLetterChangedListener(letter -> {
+            int position = mCityAdapter.getLetterPosition(letter);
+            mListView.setSelection(position);
         });
         mCityAdapter = new CityListAdapter(this);
         mListView.setAdapter(mCityAdapter);
+        initCity();
+    }
+
+    private void initCity() {
+
+        PermissionUtils.permissionAny(this, granted -> {
+                    if (granted) {
+                        getCurrentCity();
+                    }
+                },
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        );
+
+    }
+
+    private void getCurrentCity() {
+
+
+        LocationUtil.getInstance().setAddressCallback(new LocationUtil.AddressCallback() {
+            @Override
+            public void onGetAddress(Address address) {
+                String countryName = address.getCountryName();//国家
+                String adminArea = address.getAdminArea();//省
+                String locality = address.getLocality();//市
+                String subLocality = address.getSubLocality();//区
+                String featureName = address.getFeatureName();//街道
+                mCityAdapter.updateLocateState(LocateState.SUCCESS, locality, "0" + "");
+            }
+
+            @Override
+            public void onGetLocation(double lat, double lng) {
+
+            }
+        });
+
     }
 
     public void getCityData() {
@@ -146,7 +143,6 @@ public class CityPickerActivity extends BaseActivity implements GoogleApiClient.
                 //按照字母排序
                 Collections.sort(cities, (cityResponse, t1) -> cityResponse.getPinyin().compareTo(t1.getPinyin()));
                 mCityAdapter.setData(cities);
-
             }
         });
     }
@@ -168,7 +164,8 @@ public class CityPickerActivity extends BaseActivity implements GoogleApiClient.
             @Override
             public void onLocateClick() {//点击定位按钮
                 mCityAdapter.updateLocateState(LocateState.LOCATING, null, null);
-                checkIsGooglePlayConn();//重新定位
+//                checkIsGooglePlayConn();//重新定位
+                getCurrentCity();
             }
         });
     }
@@ -178,104 +175,5 @@ public class CityPickerActivity extends BaseActivity implements GoogleApiClient.
 
     }
 
-    public void showLoading() {
-        if (dialog != null && dialog.isShowing()) {
-        } else {
-            dialog = new CustomProgressDialog(this, "");
-//            dialog.setCancelable(isCancelable);
-            dialog.show();
-        }
-    }
 
-    public void stopLoading() {
-        try {
-            if (dialog != null && dialog.isShowing()) {
-                dialog.dismiss();
-                dialog = null;
-            }
-
-        } catch (Exception e) {
-
-        }
-    }
-
-    private void checkIsGooglePlayConn() {
-        if (mGoogleApiClient.isConnected() && mLastLocation != null) {
-            initArea(mLastLocation.getLatitude() + "", mLastLocation.getLongitude() + "");
-        }
-        mAddressRequested = true;
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(CityPickerActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, READ_LOCATIONCODE);
-            ActivityCompat.requestPermissions(CityPickerActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, READ_CODE);
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            lastLatLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            if (!Geocoder.isPresent()) {
-                Toast.makeText(this, "No geocoder available", Toast.LENGTH_LONG).show();
-                return;
-            }
-            if (mAddressRequested) {
-                initArea(mLastLocation.getLatitude() + "", mLastLocation.getLongitude() + "");
-            }
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-
-    private void initArea(String lat, String lng) {
-        showLoading();
-        HttpUtils.doGoogleMapReqeust("GET", "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyDjaCnD0cWNtAOPiS_Kbb5FRZ4k4qyhayk", null, new HttpUtils.StringCallback() {
-            @Override
-            public void onSuccess(int code, String result) {
-                stopLoading();
-                if (code == 200) {
-                    GoogleMapBean googleMapBean = new Gson().fromJson(result, GoogleMapBean.class);
-                    if (googleMapBean.getResults().get(0) != null) {
-                        if (googleMapBean.getResults().get(0).getAddress_components().get(2) != null) {
-                            String usecity = googleMapBean.getResults().get(0).getAddress_components().get(2).getLong_name();
-                            if (usecity != null) {
-                                if (usecity.contains("City of ")) {
-                                    usecity = usecity.substring(8);
-                                }
-                                mCityAdapter.updateLocateState(LocateState.SUCCESS, usecity, "0" + "");
-                            }
-                        }
-                    }
-                } else {
-                    ErrorBean errorBean = new Gson().fromJson(result, ErrorBean.class);
-                    showToast(errorBean.message);
-                }
-            }
-
-            @Override
-            public void onFaileure(int code, Exception e) {
-                stopLoading();
-//                showToast(e.getMessage());
-            }
-        });
-
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        mGoogleApiClient.disconnect();
-        super.onDestroy();
-    }
 }
---
+++ app/src/main/java/com/jcs/where/hotel/activity/picker/CityPickerPresenter.kt
@@ -0,0 +1,10 @@
+package com.jcs.where.hotel.activity.picker
+
+import com.jcs.where.api.network.BaseMvpPresenter
+
+/**
+ * Created by Wangsw  2021/5/13 14:07.
+ *
+ */
+class CityPickerPresenter (val view: CityPickerView) : BaseMvpPresenter(view) {
+}

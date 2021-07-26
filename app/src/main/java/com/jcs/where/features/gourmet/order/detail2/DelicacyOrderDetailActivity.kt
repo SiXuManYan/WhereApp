b78f3aab9223af1ff4c99b8116cde232c95a3642
wangsw <itshiwei@aliyun.com>
2021-07-26 17:52:23 +0800

    feat:新版美食订单详情UI

--- app/src/main/java/com/jcs/where/features/gourmet/order/detail2/DelicacyOrderDetailActivity.kt
+++ app/src/main/java/com/jcs/where/features/gourmet/order/detail2/DelicacyOrderDetailActivity.kt
@@ -1,9 +1,46 @@
 package com.jcs.where.features.gourmet.order.detail2
 
+import android.graphics.Color
+import com.blankj.utilcode.util.BarUtils
+import com.jcs.where.R
+import com.jcs.where.base.mvp.BaseMvpActivity
+import com.jcs.where.customer.ExtendChatActivity
+import com.jcs.where.utils.Constant
+import kotlinx.android.synthetic.main.activity_delicacy_order_detail.*
+
 /**
  * Created by Wangsw  2021/7/23 15:59.
  * 美食订单详情
  */
-class DelicacyOrderDetailActivity {
+class DelicacyOrderDetailActivity :BaseMvpActivity<DelicacyOrderDetailPresenter>(),DelicacyOrderDetailView{
+
+    private var orderId = "";
+
+    override fun getLayoutId() = R.layout.activity_delicacy_order_detail
+
+    override fun initView() {
+        BarUtils.setStatusBarColor(this, Color.WHITE)
+        val bundle = intent.extras
+        if (bundle == null) {
+            finish()
+            return
+        }
+        orderId = bundle.getString(Constant.PARAM_ORDER_ID, "")
+    }
+
+    override fun initData() {
+        presenter = DelicacyOrderDetailPresenter(this)
+        presenter.getDetail(orderId)
+    }
+
+    override fun bindListener() {
+        back_iv.setOnClickListener {
+            finish()
+        }
+        service_iv.setOnClickListener {
+            startActivity(ExtendChatActivity::class.java)
+        }
+
+    }
 
 }
--- app/src/main/java/com/jcs/where/features/gourmet/order/detail2/DelicacyOrderDetailComponent.kt
+++ app/src/main/java/com/jcs/where/features/gourmet/order/detail2/DelicacyOrderDetailComponent.kt
@@ -1,7 +1,9 @@
 package com.jcs.where.features.gourmet.order.detail2
 
+import com.jcs.where.api.network.BaseMvpObserver
 import com.jcs.where.api.network.BaseMvpPresenter
 import com.jcs.where.api.network.BaseMvpView
+import com.jcs.where.api.response.gourmet.order.FoodOrderDetail
 
 /**
  * Created by Wangsw  2021/7/23 16:06.
@@ -9,8 +11,22 @@ import com.jcs.where.api.network.BaseMvpView
  */
 interface DelicacyOrderDetailView : BaseMvpView {
 
+    fun bindDetail(it: FoodOrderDetail)
+
+
 }
 
-class DelicacyOrderDetail(private var view: DelicacyOrderDetailView) : BaseMvpPresenter(view) {
+class DelicacyOrderDetailPresenter(private var view: DelicacyOrderDetailView) : BaseMvpPresenter(view) {
+
+
+    fun getDetail(orderId: String) {
+        requestApi(mRetrofit.getFoodOrderDetail(orderId), object : BaseMvpObserver<FoodOrderDetail>(view) {
+            override fun onSuccess(response: FoodOrderDetail?) {
+                response?.let {
+                    view.bindDetail(it)
+                }
+            }
+        })
+    }
 
 }

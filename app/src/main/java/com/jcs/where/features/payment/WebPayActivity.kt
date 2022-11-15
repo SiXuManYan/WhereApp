627f72601d6ffa0a3ff499584353098e2165ce6b
wangsw <itshiwei@aliyun.com>
2022-11-15 14:45:45 +0800

    refactor：网页支付自定义错误页面

--- app/src/main/java/com/jcs/where/features/payment/WebPayActivity.kt
+++ app/src/main/java/com/jcs/where/features/payment/WebPayActivity.kt
@@ -3,8 +3,8 @@ package com.jcs.where.features.payment
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.net.http.SslError
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.*
 import com.jcs.where.R
@@ -30,6 +30,8 @@ class WebPayActivity : BaseMvpActivity<WebParPresenter>(), WebPayView {
 
     /** 上次未完成支付的url */
     private var lastPayUrl = ""
+
+    private var webError = false
 
     override fun isStatusDark() = true
 
@@ -70,21 +72,62 @@ class WebPayActivity : BaseMvpActivity<WebParPresenter>(), WebPayView {
             setEmptyHint(R.string.pay_error_hint)
         }
 
+        initWeb()
+
+    }
+
+    private fun initWeb() {
         web_view.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
                 empty_view.visibility = View.GONE
             }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+                webError = true
+
+            }
+
+            override fun onReceivedHttpError(view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?) {
+                super.onReceivedHttpError(view, request, errorResponse)
+                webError = true
+            }
+
+
         }
         web_view.webChromeClient = object : WebChromeClient() {
+
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+                if (!title.isNullOrBlank()) {
+                    mJcsTitle.setMiddleTitle(title)
+                } else {
+                    mJcsTitle.setMiddleTitle(getString(R.string.pay_order_title))
+                }
+            }
+
             override fun onProgressChanged(view: WebView?, newProgress: Int) {
                 super.onProgressChanged(view, newProgress)
                 if (newProgress == 100) {
-                    empty_view.visibility = View.GONE
+
+                    if (webError) {
+                        empty_view.apply {
+                            setEmptyImage(R.mipmap.ic_empty_system_upgrade)
+                            empty_message_tv.visibility = View.GONE
+                            setEmptyHint(R.string.pay_error_hint_system_update)
+
+                        }
+                        empty_view.visibility = View.VISIBLE
+                        empty_view.showEmptyContainer()
+                    } else {
+                        empty_view.visibility = View.GONE
+                    }
                 }
             }
-        }
 
+        }
     }
 
     private fun initExtra() {
@@ -116,23 +159,12 @@ class WebPayActivity : BaseMvpActivity<WebParPresenter>(), WebPayView {
         if (lastPayUrl.isBlank()) {
             showLoadingDialog()
             presenter.getPayUrl(moduleType, orderIds)
-        }else {
+        } else {
             web_view.loadUrl(lastPayUrl)
         }
     }
 
     override fun bindListener() {
-        web_view.webChromeClient = object :WebChromeClient(){
-            override fun onReceivedTitle(view: WebView?, title: String?) {
-                super.onReceivedTitle(view, title)
-                if (!title.isNullOrBlank()) {
-                    mJcsTitle.setMiddleTitle(title)
-                }else {
-                    mJcsTitle.setMiddleTitle(getString(R.string.pay_order_title))
-                }
-            }
-        }
-
         mJcsTitle.setBackIvClickListener {
             onBackPressed()
         }
@@ -141,14 +173,15 @@ class WebPayActivity : BaseMvpActivity<WebParPresenter>(), WebPayView {
     override fun bindUrl(redirectUrl: String) {
         lastPayUrl = redirectUrl
         dismissLoadingDialog()
-        web_view.loadUrl(redirectUrl)
+//        web_view.loadUrl(redirectUrl)
+        web_view.loadUrl("redirectUrl")
     }
 
     override fun onBackPressed() {
         startActivity(WebPayResultActivity::class.java, Bundle().apply {
             putInt(Constant.PARAM_TYPE, useType)
             putString(Constant.PARAM_MODULE_TYPE, moduleType)
-            putString(Constant.PARAM_LAST_PAY_URL,lastPayUrl)
+            putString(Constant.PARAM_LAST_PAY_URL, lastPayUrl)
             putIntegerArrayList(Constant.PARAM_ORDER_IDS, orderIds)
         })
         super.onBackPressed()
